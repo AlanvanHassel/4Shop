d87a6723465bcc587394c8c116a5be5ecba83c31
AlanvanHassel <Alans-place2chill@hotmail.com>
2021-10-11 23:19:15 +0200

    Product titel uit elkaar gezet

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -8,7 +8,10 @@
 				<img src="{{ url($product->image ?? 'img/placeholder.jpg') }}" alt="{{ $product->title }}" class="rounded">
 				<div class="product-body">
 					<div>
-						<h5 class="product-title"><span>{{ $product->title }}</span><em>&euro;{{ $product->price }}</em></h5>
+						<div class="product-title">
+							<h5><span>{{ $product->title }}</span></h5>
+							<h5><em>&euro;{{ $product->price }}</em></h5>
+						</div>
 						@unless(empty($product->description))
 							<p>{{ $product->description }}</p>
 						@endunless
